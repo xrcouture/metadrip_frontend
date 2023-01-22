120099b8a9e9a50479dd042671696c7bc37d763f
Nish786 <rameshananthi1969@gmail.com>
2023-01-22 20:45:45 +0530

    Texts in Utilites is Changed

--- src/component/section3/socail/Social.js
+++ src/component/section3/socail/Social.js
@@ -40,7 +40,7 @@ function Social() {
         <h1 className="section3-title-new-2 text-center section2-title-right" style={{fontFamily:"Clash Display bold"}}>Social Media Integrations.</h1>
             <p className="section3-subtitle-new-1 section2-title-right text-center" style={{fontFamily:"Clash Display "}}>
               Create reels with <b>Snapchat AR,</b>
-                <br />wear it on your photos and even<br /> on
+                <br />wear it on your <br /> 
               video calls. Get custom<br /> <b>‘virtual-fitting’</b> on a photograph to<br/>
               flaunt on your social media <br />platforms.
             </p>
@@ -72,8 +72,8 @@ function Social() {
                   <img src={passivelg3} alt="" className="passivelg5" />
                   <img src={passivelg1} alt="" className="passivelg4" />
                     <img src={passive1} alt="" className="passive1-img" />
-                    <p className='text-center passive-title pt-4' style={{fontFamily:"Clash Display Bold"}}><b>“{" "}&nbsp;  Passive Income</b></p>
-                    <p className='text-center passive-title pb-4' style={{fontFamily:"Clash Display Bold"}}><b>Multiple Possibilities.</b></p>
+                    <p className='text-center passive-title pt-4' style={{fontFamily:"Clash Display Bold"}}><b>Earn passive income</b></p>
+                    {/* <p className='text-center passive-title pb-4' style={{fontFamily:"Clash Display Bold"}}><b>Multiple Possibilities.</b></p> */}
                     <img src={passive2} alt="" className="passive2-img" />
                     <div className='passive-subtitle-container text-center'>
                     <p className='text-center passive-subtitle' style={{fontFamily:"Clash Display"}}>The holders of Meta Drip will be able to earn Passive Income. XR Couture will list the Meta Drip collection for sale on web2 platforms, such as Roblox, Zepeto, etc. The revenue earned from these platforms will be shared amongst the holders.
