b9656c77da6740d87e4f906b83b339a3aaec2085
Nish786 <rameshananthi1969@gmail.com>
2023-02-03 15:38:57 +0530

    react ga added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, {useState} from 'react'
+import React, {useState,useEffect} from 'react'
 import './App.css'
 import 'swiper/swiper.min.css'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Homepage from './Pages/Homepage';
 import NotFound from './Pages/NotFound';
+import ReactGA from 'react-ga';
+
 
 import 'react-toastify/dist/ReactToastify.css';
 import { Context } from './Context';
@@ -46,9 +48,12 @@ import ProtectedRoutes from './ProtectedRoutes';
 
 
 function App() {
-
+  const TRACKING_ID = "G-L0VT29HWGG";
+  ReactGA.initialize(TRACKING_ID);
   const [walletAddress, setWalletAddress] = useState("")
-
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
     <Context.Provider value={{walletAddress, setWalletAddress}}>
         {/* <RouterProvider router={router} /> */}
